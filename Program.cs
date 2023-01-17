5fb992393022a14a8a5dcf58d94239992284d192
carlostanaka2012 <carlostanaka2012@gmail.com>
2023-01-17 09:48:23 -0300

    estrutura

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System;
+
+//namespace Application
+//{
+    //class Program{ 
+        //static void Main(string[] args)
+       // {
+        //Console.WriteLine("Welcome, World!");
+        //}
+   // }
+//}
+
+Console.WriteLine("Welcome, World!");
+
+//var showMessage = new ShowMessage();
+//showMessage.Writeline();
+
+
+
+
+
